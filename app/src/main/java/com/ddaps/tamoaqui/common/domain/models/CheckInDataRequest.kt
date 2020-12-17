9de3f765189551bf6a7e5a59ec461a32b0bbd7ac
Vitor Vieira <nun720@gmail.com>
2020-12-17 18:10:23 -0300

    Criação das models para retorno da api - Para as chamadas de getEvents e postCheckIn

---
+++ app/src/main/java/com/ddaps/tamoaqui/common/domain/models/CheckInDataRequest.kt
@@ -0,0 +1,6 @@
+package com.ddaps.tamoaqui.common.domain.models
+
+class CheckInDataRequest(val event_id: Int,
+                         val user_name: String,
+                         val email: String){
+}
---
+++ app/src/main/java/com/ddaps/tamoaqui/common/domain/models/CheckInDataResponseOfRequest.kt
@@ -0,0 +1,4 @@
+package com.ddaps.tamoaqui.common.domain.models
+
+data class CheckInDataResponseOfRequest(val id: Int) {
+}

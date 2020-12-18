d461699113f17e09be964bc148c8c98712803373
Vitor Vieira <nun720@gmail.com>
2020-12-18 12:23:01 -0300

    Criada função para carregamento das imagens - Utilizando o @BindingAdapter podemos usar todos os beneficios do databinding e definir a chamada da função direto no xml, facilitando futuros testes

--- app/src/main/java/com/ddaps/tamoaqui/util/Extensions.kt
+++ app/src/main/java/com/ddaps/tamoaqui/util/Extensions.kt
@@ -1,5 +1,11 @@
 package com.ddaps.tamoaqui.util
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.ddaps.tamoaqui.R
 import com.ddaps.tamoaqui.common.domain.models.Event
 import com.ddaps.tamoaqui.common.domain.models.EventDataResponse
 
@@ -19,3 +25,13 @@ fun List<EventDataResponse>.mapForView(): List<Event>{
 }
 
 
+@BindingAdapter("imageUrl")
+fun ImageView.load(imagemUrl: String?) {
+    val imageView = this
+    val errorImage = imageView.context.resources.getDrawable(R.drawable.errorimage)
+    val requestOptions = Glide.with(imageView.context)
+                              .applyDefaultRequestOptions(RequestOptions().error(errorImage))
+                              .load(imagemUrl)
+                              .transition(DrawableTransitionOptions.withCrossFade(299))
+    requestOptions.into(imageView)
+}
